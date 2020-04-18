73482ba54ec7768b95e5e2f6ff2b0d6e025c27a0
vessel <xie.can@outlook.com>
2020-04-18 10:51:06 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace TrojanWrapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TrojanWrapper = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.autoStratupMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TrojanWrapper
+            // 
+            this.TrojanWrapper.ContextMenuStrip = this.contextMenuStrip1;
+            this.TrojanWrapper.Icon = ((System.Drawing.Icon)(resources.GetObject("TrojanWrapper.Icon")));
+            this.TrojanWrapper.Text = "TrojanWrapper";
+            this.TrojanWrapper.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.autoStratupMenu,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(124, 52);
+            // 
+            // autoStratupMenu
+            // 
+            this.autoStratupMenu.Name = "autoStratupMenu";
+            this.autoStratupMenu.Size = new System.Drawing.Size(123, 24);
+            this.autoStratupMenu.Text = "自启动";
+            this.autoStratupMenu.Click += new System.EventHandler(this.autoStratupMenu_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(123, 24);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon TrojanWrapper;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autoStratupMenu;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrojanWrapper
+{
+    public delegate void DelReadStdOutput(string result);
+    public delegate void DelReadErrOutput(string result);
+    public partial class Form1 : Form
+    {
+        public event DelReadStdOutput ReadStdOutput;
+        public event DelReadErrOutput ReadErrOutput;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            ReadStdOutput += new DelReadStdOutput(ReadStdOutputAction);
+            ReadErrOutput += new DelReadErrOutput(ReadErrOutputAction);
+            this.Opacity = 0;
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           // KillTrojan();
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hide();
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+
+            string exePath = Process.GetCurrentProcess().MainModule.FileName;
+            string exeName = Process.GetCurrentProcess().MainModule.ModuleName;
+            var value = reg.GetValue(exeName);
+            if (value != null)
+            {   
+                autoStratupMenu.Checked = true;
+                if (!value.ToString().Equals(exePath))
+                {
+                    reg.SetValue(exeName, exePath);
+                }
+            }
+
+            System.IO.Directory.SetCurrentDirectory(Application.StartupPath);
+            // MessageBox.Show(System.IO.Directory.GetCurrentDirectory());
+            //  MessageBox.Show(Application.StartupPath);
+            Process cmdProcess = new Process();
+            cmdProcess.StartInfo.FileName = "trojan.exe";      // 命令
+
+            cmdProcess.StartInfo.CreateNoWindow = true;         // 不创建新窗口
+            cmdProcess.StartInfo.UseShellExecute = false;
+            cmdProcess.StartInfo.RedirectStandardInput = true;  // 重定向输入
+            cmdProcess.StartInfo.RedirectStandardOutput = true; // 重定向标准输出
+            cmdProcess.StartInfo.RedirectStandardError = true;  // 重定向错误输出
+            //CmdProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            cmdProcess.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+            cmdProcess.ErrorDataReceived += new DataReceivedEventHandler(p_ErrorDataReceived);
+
+            cmdProcess.EnableRaisingEvents = true;                      // 启用Exited事件
+            cmdProcess.Exited += new EventHandler(CmdProcess_Exited);   // 注册进程结束事件
+
+            cmdProcess.Start();
+            cmdProcess.BeginOutputReadLine();
+            cmdProcess.BeginErrorReadLine();
+
+            Process privoxyProcess = new Process
+            {
+                // Configure the process using the StartInfo properties.
+                StartInfo =
+                    {
+                        FileName = "privoxy.exe",
+                        Arguments = "config.txt",
+                      
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        UseShellExecute = true,
+                        CreateNoWindow = true
+                    }
+            };
+            privoxyProcess.Start();
+
+        }
+
+        private void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                // 4. 异步调用，需要invoke
+                this.Invoke(ReadStdOutput, new object[] { e.Data });
+            }
+        }
+
+        private void p_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                this.Invoke(ReadErrOutput, new object[] { e.Data });
+            }
+        }
+
+        private void ReadStdOutputAction(string result)
+        {
+           
+        }
+
+        private void ReadErrOutputAction(string result)
+        {
+             
+        }
+
+        private void CmdProcess_Exited(object sender, EventArgs e)
+        {
+            // 执行结束后触发
+        }
+
+        private void autoStratupMenu_Click(object sender, EventArgs e)
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+
+            string exePath = Process.GetCurrentProcess().MainModule.FileName;
+            string exeName = Process.GetCurrentProcess().MainModule.ModuleName;
+            if (autoStratupMenu.Checked)
+            {
+                reg.DeleteValue(exeName);
+                autoStratupMenu.Checked = false;
+               
+            }
+            else
+            {
+                reg.SetValue(exeName, exePath);
+                autoStratupMenu.Checked = true;
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+           Hide();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Kill();
+        }
+
+        private void Kill()
+        {
+            
+            string[] names = new string[] { "trojan", "privoxy" };
+            
+            var pro = Process.GetProcesses().Where(p=>names.Contains(p.ProcessName.ToLower()));
+
+            foreach (Process p in pro)
+            {
+                p.Kill();
+            }
+
+
+        }
+    }
+}
